1791df3c6b5fa2f0cb2d4c4e30d9c81d8da4d041
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-04-30 13:51:18 -0400

    Update CountFormTypeIconProvider.tsx

--- src/components/CountFormTypeIconProvider.tsx
+++ src/components/CountFormTypeIconProvider.tsx
@@ -56,7 +56,7 @@ const CountFormTypeIconsProvider = (props: CountFormTypeIconsProviderProps) => {
         return iconData.svgPathDefinition;
     }
 
-    const getFormTypeIcon = (formType: FormType) => {
+    const getCountFormTypeIconData = (formType: FormType) => {
         switch (formType.formType) {
             case 0:
                 return getIconData(formType, "icon0");
@@ -68,7 +68,7 @@ const CountFormTypeIconsProvider = (props: CountFormTypeIconsProviderProps) => {
     return (
         <CountFormTypeIconsContext.Provider value={{
             countFormTypeIconData,
-            getCountFormTypeIconData: getFormTypeIcon,
+            getCountFormTypeIconData: getCountFormTypeIconData,
         }}>
             {children}
             <button onClick={() => setShowSvg(!showSvg)}>Show SVG</button>
