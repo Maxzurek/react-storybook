a0e593d70151e6eeb74a2f41041f1ce546b90f73
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-06-02 23:13:02 -0400

    Revert "Revert "Revert "Bunch of css"""
    This reverts commit e73e61094e47646588435129ffe6af59365b41e3.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
-    <StrictMode>
-        <StorylineProvider>
-            <App />
-        </StorylineProvider>
-    </StrictMode>
+    <StorylineProvider>
+        <App />
+    </StorylineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
