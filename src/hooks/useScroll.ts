3538c4fbc9c6770ef037508ca2340addcc74d2d9
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-08-20 12:00:57 -0400

    Update useScroll -Now properly update scrollHeight state

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -10,29 +10,40 @@ const useScroll = (divRef: MutableRefObject<HTMLDivElement>) => {
     const [scrollPosition, setScrollPosition] = useState(0);
     const [scrollHeight, setScrollHeight] = useState(0);
 
+    const divElement = useRef<HTMLDivElement>(null);
     const scrollPositionRef = useRef(0);
+    const lastScrollHeightRef = useRef(0);
 
     const handleScroll = useCallback(() => {
         const position = divRef.current?.scrollTop;
+        const divHeight =
+            (divElement.current?.scrollHeight ?? 0) -
+            (divElement.current?.offsetHeight ?? 0);
 
         if (position !== scrollPositionRef.current) {
             setScrollPosition(divRef.current?.scrollTop ?? 0);
             scrollPositionRef.current = divRef.current?.scrollTop ?? 0;
         }
+
+        if (lastScrollHeightRef.current !== divHeight) {
+            setScrollHeight(divHeight);
+        }
     }, [divRef]);
 
     useEffect(() => {
-        const divElement = divRef.current;
+        divElement.current = divRef.current;
 
-        divElement?.addEventListener("scroll", handleScroll, {
+        divElement.current?.addEventListener("scroll", handleScroll, {
             passive: true,
         });
 
         setScrollHeight(
-            (divElement?.scrollHeight ?? 0) - (divElement?.offsetHeight ?? 0)
+            (divElement.current?.scrollHeight ?? 0) -
+                (divElement.current?.offsetHeight ?? 0)
         );
 
-        return () => divElement?.removeEventListener("scroll", handleScroll);
+        return () =>
+            divElement.current?.removeEventListener("scroll", handleScroll);
     }, [divRef, handleScroll]);
 
     return { scrollPosition, scrollHeight };
