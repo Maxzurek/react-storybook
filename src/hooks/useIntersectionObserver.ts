c954448672c5ae60611c3ca170a7c6870db99941
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-09-23 16:21:54 -0400

    Update useIntersectionObserver  - Add intersectionThreshold to the onNodeIntersecting params

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -7,31 +7,36 @@ function useIntersectionObserver<T extends HTMLElement>() {
     /**
      * Observer the node provided using the IntersectionObserver.
      * As soon as the node is intersecting, the action callback provided will be invoked and the observer will be disconnected.
-     * @param node
-     * @param action
+     *
+     * @param node - the element to observe
+     * @param action callback
+     * @param intersectionThreshold default: 0.01 (which means the action callback will be invoked if at least 1% of the node is visible in the viewport)
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
      */
-    const onNodeIntersecting = useCallback((node: T, action: (node: T) => void) => {
-        const observerCallback: IntersectionObserverCallback = (entries, observer) => {
-            const entry = entries?.[0];
+    const onNodeIntersecting = useCallback(
+        (node: T, action: (node: T) => void, intersectionThreshold = 0.1) => {
+            const observerCallback: IntersectionObserverCallback = (entries, observer) => {
+                const entry = entries?.[0];
 
-            if (entry.isIntersecting) {
-                observer.disconnect();
-                isIntersectingRef.current = true;
-                action(node);
-            }
-        };
+                if (entry.isIntersecting) {
+                    observer.disconnect();
+                    isIntersectingRef.current = true;
+                    action(node);
+                }
+            };
 
-        observer.current?.disconnect();
-        observer.current = new IntersectionObserver(observerCallback, {
-            root: null, // use the viewport
-            rootMargin: "0px",
-            threshold: 1.0,
-        });
+            observer.current?.disconnect();
+            observer.current = new IntersectionObserver(observerCallback, {
+                root: null, // use the viewport
+                rootMargin: "0px",
+                threshold: intersectionThreshold,
+            });
 
-        observer.current.observe(node);
-    }, []);
+            observer.current.observe(node);
+        },
+        []
+    );
 
     const isIntersecting = isIntersectingRef.current;
 
