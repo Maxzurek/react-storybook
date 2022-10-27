668627f91045fe72da1d250b4c00c679b1051b5a
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-10-26 22:23:30 -0400

    Update Toggle  - Add toggleAnimationDuration prop

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -1,57 +1,79 @@
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 import "./Toggle.scss";
 
 export type ToggleSize = "small" | "medium" | "large";
 
-interface ToggleProps {
+interface ToggleProps
+    extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     isOn?: boolean;
     size?: ToggleSize;
     htmlFor?: string;
     isDisabled?: boolean;
-
+    /**
+     * The animation duration of the sliding effect in milliseconds.
+     * @default 250
+     */
+    toggleAnimationDuration?: number;
     onToggle?: (isOn: boolean) => void;
 }
 
-Toggle.defaultProps = {
-    size: "small",
-};
+const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
+    (
+        {
+            isOn: isOnControlled,
+            size = "small",
+            htmlFor,
+            isDisabled,
+            toggleAnimationDuration = 250,
+            onToggle,
+            ...inputProps
+        },
+        ref
+    ) => {
+        const [isOn, setIsOn] = useState(false);
 
-export default function Toggle({
-    isOn: isOnControled,
-    size,
-    htmlFor,
-    isDisabled,
-    onToggle,
-}: ToggleProps) {
-    const [isOn, setIsOn] = useState(false);
+        const handleToggleChanged = (isOn: boolean) => {
+            onToggle?.(isOn);
+            isOnControlled ?? setIsOn(isOn);
+        };
 
-    const handleToggleChanged = (isOn: boolean) => {
-        onToggle?.(isOn);
-        isOnControled ?? setIsOn(isOn);
-    };
+        const switchClassNames = ["toggle__switch"];
+        size === "medium" && switchClassNames.push("toggle__switch--md");
+        size === "large" && switchClassNames.push("toggle__switch--lg");
+        isDisabled && switchClassNames.push("toggle__switch--disabled");
 
-    const switchClassNames = ["toggle__switch"];
-    size === "medium" && switchClassNames.push("toggle__switch--md");
-    size === "large" && switchClassNames.push("toggle__switch--lg");
-    isDisabled && switchClassNames.push("toggle__switch--disabled");
+        const sliderClassNames = ["toggle__slider"];
+        size === "medium" && sliderClassNames.push("toggle__slider--md");
+        size === "large" && sliderClassNames.push("toggle__slider--lg");
+        isDisabled && sliderClassNames.push("toggle__slider--disabled");
 
-    const sliderClassNames = ["toggle__slider"];
-    size === "medium" && sliderClassNames.push("toggle__slider--md");
-    size === "large" && sliderClassNames.push("toggle__slider--lg");
-    isDisabled && sliderClassNames.push("toggle__slider--disabled");
+        const handleToggleRefCallback = (node: HTMLInputElement) => {
+            if (node) {
+                node.style.setProperty(
+                    "--toggle-animation-duration",
+                    `${toggleAnimationDuration / 1000}s`
+                );
+            }
+        };
 
-    return (
-        <div className="toggle">
-            <label className={switchClassNames.join(" ")}>
-                <input
-                    checked={isOnControled ? isOnControled : isOn}
-                    disabled={isDisabled}
-                    id={htmlFor}
-                    type="checkbox"
-                    onChange={(e) => handleToggleChanged(e.target.checked)}
-                />
-                <span className={sliderClassNames.join(" ")}></span>
-            </label>
-        </div>
-    );
-}
+        return (
+            <div ref={handleToggleRefCallback} className="toggle">
+                <label className={switchClassNames.join(" ")}>
+                    <input
+                        ref={ref}
+                        checked={isOnControlled ? isOnControlled : isOn}
+                        disabled={isDisabled}
+                        id={htmlFor}
+                        type="checkbox"
+                        onChange={(e) => handleToggleChanged(e.target.checked)}
+                        {...inputProps}
+                    />
+                    <span className={sliderClassNames.join(" ")}></span>
+                </label>
+            </div>
+        );
+    }
+);
+
+Toggle.displayName = "Toggle";
+export default Toggle;
