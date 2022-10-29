fbe2a72b9a006c000eb3436067ca56d39ce5ac7e
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-10-29 16:00:55 -0400

    Add ObjectUtils

---
+++ src/utilities/ObjectUtils.ts
@@ -0,0 +1,22 @@
+const areEqualShallow = (object1: object, object2: object) => {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+
+    if (!keys1.length && !keys2.length) {
+        return true;
+    }
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (const key in object1) {
+        const keyAsIndex = key as keyof typeof object1;
+        if (object1[keyAsIndex] !== object2[keyAsIndex]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+export { areEqualShallow };
