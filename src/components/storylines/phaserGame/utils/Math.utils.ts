65e3da218b35a83ec359b92dc52ed08f8b63ce5c
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2023-03-12 01:50:21 -0500

    Update Math.utils  - Add millisecondsToSeconds function

--- src/components/storylines/phaserGame/utils/Math.utils.ts
+++ src/components/storylines/phaserGame/utils/Math.utils.ts
@@ -2,4 +2,8 @@ export default class MathUtils {
     static secondsToMilliseconds(seconds: number) {
         return seconds * 1000;
     }
+
+    static millisecondsToSeconds(milliseconds: number) {
+        return milliseconds / 1000;
+    }
 }
