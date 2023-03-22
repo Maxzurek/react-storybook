3f2637e19ee9b17e6969b7a727733421a3065f54
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2023-03-22 16:26:29 -0400

    Add react-ga4

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { StorylineProvider } from "./components/contexts/Storyline.context";
+import ReactGA from "react-ga4";
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+ReactGA.initialize([{ trackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID }]);
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <StorylineProvider>
         <React.StrictMode>
